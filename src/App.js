eac6ea78d73e7065ce2e6b01c7cb81057e55b600
solomonade1 <solomonadeboyega1@gmail.com>
2022-05-17 06:14:58 +0100

    Resolving Issues

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import {Navbar } from './components';
+
 
 import './App.scss';
-import { About, Footer, Skills, Header, Works, Testimonials } from './container';
-import {Navbar } from './components';
+import {About, Header, Footer, Works, Skills, Testimonials} from './container'
+
 
 const App = () => (
   <div className="app">
-<Navbar />
- <Header />
- <About />
- <Works />
- <Skills />
- <Testimonials />
- <Footer />
+    <Navbar />
+    <Header />
+    <About />
+    <Works />
+    <Skills />
+    <Testimonials />
+    <Footer />
+
   </div>
 );
 
--- src/client.js
+++ src/client.js
@@ -2,13 +2,14 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectId: process.env.REACT_APP__SANITY_PROJECT_ID,
+    projectId: 'tczabs4y',
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
     ignoreBrowserTokenWarning: true,
-    token: process.env.REACT_APP__SANITY_TOKEN,
+    token: 'sk9o2nDXL2bNxQgFDmGdbyOVbuyVqyHV1knuKAiJKIY8fvPZHOoQNMpuyQta0IR0pwGvLVPYhjJEcqDdXTXdgrxtCfdPyocx8ifnXRGb3hzu5dI26rUo2PPtV8NBZ5Wt3rZi84FDQmSQlT9qLCLBfS4HaZP0R36fnhQJrs9RK7vxclBUgV7n'
 });
+console.log("Project Id", process.env.REACT_APP__SANITY_PROJECT_ID)
 
 const builder = imageUrlBuilder(client);
 
